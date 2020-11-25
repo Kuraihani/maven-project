57d67063f6b524763019a28d7317373ef3040fc5
Kuraihani <55689058+Kuraihani@users.noreply.github.com>
2020-11-25 09:28:31 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -5,17 +5,17 @@ package com.example;
 /**
  * This is a class.
  */
-public class Greeter{
+public class Greeter {
   /**
    * this is a constructor.
    */
-  public Greeter(){    
+  public Greeter() {    
   }
   /**
    * @param someone name of a person
    * @return greeting string
    */   
-  public final String greet(final String someone){
+  public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
 }
